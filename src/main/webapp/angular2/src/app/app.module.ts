9b7e266c2d2b2363cb07e02e9d8a90e6e06d7ad6
Premium.Lab <24338630+Premiumlab@users.noreply.github.com>
2017-12-26 01:46:46 -0600

    check-in and check-out dates & submit functionality

--- src/main/webapp/angular2/src/app/app.module.ts
+++ src/main/webapp/angular2/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 
 import { AppComponent } from './app.component';
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
